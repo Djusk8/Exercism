d5f146367cdac535e7cfe642852a7170be4cd670
SEO <djusk8@gmail.com>
2019-07-31 15:32:32 +0300

    Solved matrix challenge

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,14 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        self.matrix = matrix_string.split('\n')  # Separate rows
+        self.matrix = [self.matrix[i].split(' ') for i in range(len(self.matrix))]  # Separate numbers in each row
+        self.matrix = [[int(i) for i in j] for j in self.matrix]  # Convert string numbers to int
 
     def row(self, index):
-        pass
+        """ Returns matrix row by index"""
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        """ Returns matrix column by index"""
+        return [self.matrix[i][index-1] for i in range(len(self.matrix))]
+
